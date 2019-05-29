f465df9537cb1d874312aedcf311860182c95bf1
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2019-05-29 23:34:33 +0200

    Add PacketParsing attribute
    * Added the PackeParsingAttribute to allow more generic parsing

---
+++ src/ZigBeeNet.Hardware.TI.CC2531/Packet/PacketParsingAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZigBeeNet.Hardware.TI.CC2531.Packet
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class PacketParsingAttribute : Attribute
+    {
+        public ZToolCMD CMD { get; }
+
+        public PacketParsingAttribute(ZToolCMD cmd)
+        {
+            CMD = cmd;
+        }
+
+    }
+}
